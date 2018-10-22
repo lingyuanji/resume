01aaab9c15ac395d21d3cfd17132e4a61b4d5ee9
Lingyuan Ji <LingyuanJi@users.noreply.github.com>
2018-10-22 16:34:24 -0400

    Update make-resume.sh

--- make-resume.sh
+++ make-resume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl "https://inspirehep.net/search?p=f+a+l.ji.7&of=hx&em=B&sf=year&so=d&rg=250" > my.bib
+curl "https://inspirehep.net/search?p=f+a+Ling.Yuan.Ji.1&of=hx&em=B&sf=year&so=d&rg=250" > my.bib
 perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' my.bib
 latexmk -C
 latexmk --xelatex resume.tex
