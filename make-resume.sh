97de955be9ec3b86e3eeaa4c6cddda6c5c887a2b
Lingyuan Ji <lingyuan.ji@outlook.com>
2019-05-07 17:47:47 -0400

    added name highlighting

--- make-resume.sh
+++ make-resume.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl "https://inspirehep.net/search?p=f+a+Ling.Yuan.Ji.1&of=hx&em=B&sf=year&so=d&rg=250" > my.bib
 perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' my.bib
+perl -i -pe 's/Ji,\ Lingyuan/\\textbf{Ji},\ \\textbf{Lingyuan}/g' my.bib
 latexmk -C
 latexmk --xelatex resume.tex
