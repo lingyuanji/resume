f1a9785a38efe79aefb8ff1e7eb91305841492e2
Lingyuan Ji <lingyuan.ji@outlook.com>
2019-08-29 13:12:08 -0400

    * changed make-resume.sh cleaning * changed paper size

--- make-resume.sh
+++ make-resume.sh
@@ -2,5 +2,5 @@
 curl "https://inspirehep.net/search?p=f+a+Ling.Yuan.Ji.1&of=hx&em=B&sf=earliestdate&so=d&rg=250" > my.bib
 perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' my.bib
 perl -i -pe 's/Ji,\ Lingyuan/\\textbf{Ji},\ \\textbf{Lingyuan}/g' my.bib
-latexmk -C
 latexmk --xelatex resume.tex
+latexmk -c
