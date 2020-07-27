14a15e5490a0ef4241559e14843d330150996e89
Lingyuan Ji <lingyuan.ji@outlook.com>
2020-07-27 18:40:34 -0400

    updated the query URL to be compatible with the newest inspirehep API: https://blog.inspirehep.net/2020/06/we-released-the-new-inspire-api/; added shell scripts for cleaning

--- make-resume.sh
+++ make-resume.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl "https://old.inspirehep.net/search?p=f+a+Ling.Yuan.Ji.1&of=hx&em=B&sf=earliestdate&so=d&rg=250" > my.bib
+curl "https://inspirehep.net/api/literature?q=find+author+ling.yuan.ji.1&format=bibtex&sort=mostrecent&size=1000" > my.bib
 perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' my.bib
 perl -i -pe 's/Ji,\ Lingyuan/\\textbf{Ji},\ \\textbf{Lingyuan}/g' my.bib
 latexmk --xelatex resume.tex
 latexmk -c
+rm my.bib resume.bbl
